01c9010b6e7a820f9cb18d57790d9279ce73daf8
Samanta Evangelista <samidanieva@gmail.com>
2023-11-22 10:48:59 -0300

    Canbios en NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Bebidas</Nav.Link>
+            <Nav.Link href="#pricing">Snacks</Nav.Link>
+            <Nav.Link href="#pricing">Golosinas</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Navbar;
