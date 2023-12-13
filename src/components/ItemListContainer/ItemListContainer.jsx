cbcd6cd2259c029f3008bfc401b3427788000115
Samanta Evangelista <samidanieva@gmail.com>
2023-12-13 20:24:56 -0300

    Modificaciones

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,12 +4,22 @@ import { useParams } from "react-router-dom";
 import arrayProductos from "../components/json/arrayProductos.json";
 import ".ItemListContainer.css";
 
-const ItemListContainer = ({ greeting }) => {
-  return (
-    <div>
-      <h2>{greeting}</h2>
-    </div>
-  );
-};
+const ItemListContainer = () => {
+  const [item, setItem] = useState([])
+  const {id} = useParams();
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try{
+        const data = await new Promise
+      }
+    }  
+    await new Promise((resolve)=>{
+      ((resolve)=>{
+        setTimeout(()=>{
+        resolve( id ? arrayProductos.filter(item => item.categori === id): arrayProductos)
+      })
+    })
+  })
 
 export default ItemListContainer;
