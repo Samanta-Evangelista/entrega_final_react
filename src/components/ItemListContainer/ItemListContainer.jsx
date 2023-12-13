2f96a937909d69b85b751a0491a5d05221eb7401
Samanta Evangelista <samidanieva@gmail.com>
2023-12-13 20:24:16 -0300

    Modificaciones

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import arrayProductos from "../components/json/arrayProductos.json";
+import ".ItemListContainer.css";
 
 const ItemListContainer = ({ greeting }) => {
   return (
