782b0964ed34e7b6c3515999e0fe2e56ced95f60
Samanta Evangelista <samidanieva@gmail.com>
2023-12-13 23:56:25 -0300

    ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import arrayProductos from "../components/json/arrayProductos.json";
+import ItemList from "../components/item/Item.jsx";
+import arrayProductos from "../components/Json/arrayProductos.json";
 import ".ItemListContainer.css";
 
 const ItemListContainer = () => {
-  const [item, setItem] = useState([])
-  const {id} = useParams();
+  const [item, setItem] = useState([]);
+  const { id } = useParams();
 
-  useEffect(()=>{
-    const fetchData = async()=>{
-      try{
-        const data = await new Promise
+  useEffect(() => {
+    const fetchData = async ()=>{
+      try {
+        const data = await new Promise((resolve) => {
+          setTimeout(() => {
+            resolve( id ? arrayProductos.filter((item => item.categoria === id) , arrayProductos)
+          } , 2000);
+        });
+        setItem(data);
+      } catch (error) {
+        console.log("Error:", error);
       }
-    }  
-    await new Promise((resolve)=>{
-      ((resolve)=>{
-        setTimeout(()=>{
-        resolve( id ? arrayProductos.filter(item => item.categori === id): arrayProductos)
-      })
-    })
-  })
+    };
+    fetchData();
+  }, [id]);
 
+  return (
+    <div className="container">
+      <div className="row">
+        <ItemList item={item} />
+      </div>
+    </div>
+  );
+};
 export default ItemListContainer;
