3d8d3151ee842b550ab93947f5a6db86de98199c
Samanta Evangelista <samidanieva@gmail.com>
2023-12-13 23:56:54 -0300

    ItemList

---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Item from "../Item/Item";
+
+const ItemList = ({ item }) => {
+  return (
+    <div className="row">
+      {nombre.map((item) => (
+        <div className="col-md3" key={Item.id}>
+          <Item item={item} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ItemList;
