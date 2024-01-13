0187a21ce0350ca76fed9c856151aacb7cdb180a
Samanta Evangelista <samidanieva@gmail.com>
2024-01-13 18:04:24 -0300

    Se agrega ItemCount

---
+++ src/components/ItemDetail/ItemCount.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./ItemCount.css";
+
+const ItemCount = ({
+  cantidad,
+  handlerRestar,
+  handlerSumar,
+  handlerAgregar,
+}) => {
+  return (
+    <div>
+      <div className="item-count">
+        <button onClick={handlerRestar}>-</button>
+        <p>{cantidad}</p>
+        <button onClick={handlerSumar}>+</button>
+      </div>
+
+      <button className="agregar-al-carrito" onClick={handlerAgregar}>
+        Agregar al carrito
+      </button>
+    </div>
+  );
+};
+
+export default ItemCount;
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import ItemCount from "./ItemCount";
 
 const ItemDetail = ({ item }) => {
+  const [cantidad, setCantidad] = useState(1);
+
+  const handlerRestar = () => {
+    cantidad > 1 && setCantidad(cantidad - 1);
+  };
+
+  const handlerSumar = () => {
+    cantidad < item.stock && setCantidad(cantidad + 1);
+  };
+
+  const handlerAgregar = () => {
+    console.log({ ...item, cantidad });
+  };
+
   return (
     <div className="container">
       <div className="card border border-0">
@@ -13,8 +28,14 @@ const ItemDetail = ({ item }) => {
         <h3>{item.nombre}</h3>
         <h4>$ {item.precio}</h4>
         <p>
-          <strong>Cantidad: {item.stock}</strong>{" "}
+          <strong>Stock: {item.stock}</strong>{" "}
         </p>
+        <ItemCount
+          cantidad={cantidad}
+          handlerRestar={handlerRestar}
+          handlerSumar={handlerSumar}
+          handlerAgregar={handlerAgregar}
+        ></ItemCount>
       </div>
     </div>
   );
